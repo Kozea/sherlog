a0169a6a4214463de3089289da9a38522e3d3b3b
grewn0uille <lucieanglade.gm@gmail.com>
2018-02-14 14:43:56 +0100

    Change import

--- sherlog/__init__.py
+++ sherlog/__init__.py
@@ -3,19 +3,17 @@ import os
 
 from urllib.parse import urlparse
 
-from .sherlog import Sherlog
+from .app import app
 
 
 locale.setlocale(locale.LC_ALL, 'fr_FR')
 
-app = Sherlog(__name__)
-app.config.from_envvar('FLASK_CONFIG')
 
-app.initialize()
-
-
-@app.cli.command()
+# @app.cli.command()
 def drop_db():
     filename = urlparse(app.config['DB']).path
     if os.path.isfile(filename):
         os.remove(filename)
+
+
+app.cli.command()(drop_db)
--- sherlog/log_parser.py
+++ sherlog/log_parser.py
@@ -5,7 +5,7 @@ import time
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sherlog import model
+from sherlog.model import Log
 
 
 def get_config():
@@ -23,7 +23,7 @@ def get_session(config):
 
 
 def convert_to_dict(data):
-    return eval(data[1:-2].replace('\n', ''))
+    return eval(data[1:-1].replace('\n', ''))
 
 
 def get_name(data):
@@ -56,7 +56,7 @@ def insert_log(line, dbsession):
         title = line.split('(')[0]
         server_name = get_name(title)
         desc = convert_to_dict(line.split(title)[1])
-        dbsession.add(model.Log(build_log(server_name, desc)))
+        dbsession.add(Log(build_log(server_name, desc)))
 
 
 def read_log(logfile):
