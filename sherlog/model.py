1d59bc83b1c25e2c20a99513c53b7845ce8a6d6d
grewn0uille <lucieanglade.gm@gmail.com>
2018-02-26 18:25:38 +0100

    String to Unicode

--- sherlog/model.py
+++ sherlog/model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, DateTime, Integer, String
+from sqlalchemy import Boolean, Column, DateTime, Integer, String, Unicode
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -8,16 +8,16 @@ class Log(Base):
     __tablename__ = 'log'
 
     return_code = Column(Integer, nullable=True)
-    message = Column(String, nullable=False)
-    url = Column(String, nullable=True)
+    message = Column(Unicode, nullable=False)
+    url = Column(Unicode, nullable=True)
     ok = Column(Boolean, nullable=False)
-    host = Column(String, nullable=True, index=True)
+    host = Column(Unicode, nullable=True, index=True)
     start = Column(DateTime, primary_key=True)
     stop = Column(DateTime, nullable=False)
-    status = Column(String, nullable=False)
+    status = Column(Unicode, nullable=False)
 
-    stderr = Column(String, nullable=True)
-    stdout = Column(String, nullable=True)
-    command = Column(String, nullable=True)
+    stderr = Column(Unicode, nullable=True)
+    stdout = Column(Unicode, nullable=True)
+    command = Column(Unicode, nullable=True)
 
-    server_name = Column(String, nullable=False, index=True)
+    server_name = Column(Unicode, nullable=False, index=True)
