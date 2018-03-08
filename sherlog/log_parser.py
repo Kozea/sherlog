7a3768f8c788e200106a5419f3e6ba2528968580
grewn0uille <lucieanglade.gm@gmail.com>
2018-03-08 15:37:08 +0100

    Remove max stop condition on insert

--- sherlog/log_parser.py
+++ sherlog/log_parser.py
@@ -72,9 +72,8 @@ def insert_log(line, dbsession, max_stop):
         title = line.split('(')[0]
         server_name = get_name(title)
         desc = convert_to_dict(line.split(title)[1])
-        if desc['end'] > max_stop:
-            log = Log(**build_log(server_name, desc))
-            dbsession.add(log)
+        log = Log(**build_log(server_name, desc))
+        dbsession.add(log)
 
 
 def insert_missing_lines():
