d1788646dec1d1e6204a8f2b29ac695f2bc70d56
grewn0uille <lucieanglade.gm@gmail.com>
2018-08-01 11:50:51 +0200

    Ping6 graphs work again

--- sherlog/graph.py
+++ sherlog/graph.py
@@ -34,7 +34,7 @@ def get_data(server_name, ping_service, start, stop):
         data = (
             g.session.query(Log)
             .filter(Log.server_name == server_name)
-            .filter(Log.command.contains('ping6'))
+            .filter(Log.command.contains('ping -6'))
             .filter(Log.start > start)
             .all()
         )
